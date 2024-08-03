157596d3fdcd75a3f26b0ef1bec2fec5369fa487
Fratila Andrei <andifratila121618@gmail.com>
2024-08-04 00:11:29 +0300

    Added other changes

--- CLIENT/src/app/(homepage)/(components)/OurPrograms/OurPrograms.tsx
+++ CLIENT/src/app/(homepage)/(components)/OurPrograms/OurPrograms.tsx
@@ -5,22 +5,21 @@ import OurProgramsCard from './OurProgramsCard';
 
 const OurPrograms: FC = () => {
   return (
-    <div className='space-y-16 pb-16'>
-      <div className='md:text-5xl" text-center text-4xl font-bold'>
+    <section className='space-y-16'>
+      {/* Update to <FGSectionTitle /> when feature/forge-18 merges */}
+      <div className='text-center text-4xl font-bold md:text-5xl'>
         <span className='outline-text text-text-primary-normal'>Our</span>{' '}
         <span className='text-primary-normal'>Programs</span>
       </div>
       <div className='mx-4 flex flex-wrap justify-center gap-6 md:mx-0 lg:justify-between'>
-        {OUR_PROGRAMS_ITEMS.map((item: OurProgramsType) => {
-          return (
-            <OurProgramsCard
-              key={item.title}
-              item={item}
-            />
-          );
-        })}
+        {OUR_PROGRAMS_ITEMS.map((item: OurProgramsType) => (
+          <OurProgramsCard
+            key={item.title}
+            item={item}
+          />
+        ))}
       </div>
-    </div>
+    </section>
   );
 };
 
--- CLIENT/src/app/(homepage)/(components)/OurPrograms/OurProgramsCard.tsx
+++ CLIENT/src/app/(homepage)/(components)/OurPrograms/OurProgramsCard.tsx
@@ -13,13 +13,10 @@ const OurProgramsCard: FC<Props> = ({ item }) => {
       key={title}
       className='flex max-h-fit cursor-pointer flex-col items-center space-y-6 rounded-2xl border-2 border-primary-normal p-4 text-center transition-all hover:scale-105 hover:border-white hover:bg-background-dark-red sm:w-[45%] lg:w-[30%] xl:w-[23%]'>
       <div className='rounded-full border-2 border-primary-normal p-12'>
-        <item.icon
-          className='size-12 text-xl xl:size-16'
-          aria-hidden='true'
-        />
+        <item.icon className='size-12 text-xl xl:size-16' />
       </div>
       <div className='text-xl font-bold'>{title}</div>
-      <div className='text-slate-100'>{description}</div>
+      <div className='text-text-secondary'>{description}</div>
     </div>
   );
 };
